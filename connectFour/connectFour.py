4a04213f729611630c90f1e6feb2d93151f16a3b
evdb <emelinevanderbeken@gmail.com>
2021-10-31 17:56:04 +0100

    Removing help functions from main code.

--- connectFour/connectFour.py
+++ connectFour/connectFour.py
@@ -5,19 +5,22 @@ Created on Sat Jul 24 16:02:49 2021
 
 @author: Emeline
 """
+
 import tkinter as tk
 import numpy as np
 import random
+from help_GUI import about, print_rules
+
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-WIDTH=50
-LENGTH=40
+WIDTH = 50
+LENGTH = 40
 listMessage=['Click on Game to start a new game',
              'Player 1 wins. Click on Game to start a new game',
              'Player 2 wins. Click on Game to start a new game']
-difficulty_AI=1
-onePlayer=True
+difficulty_AI = 1
+onePlayer = True
 def create_board():
     board=np.zeros((6,7))
     return board
@@ -143,39 +146,7 @@ def nextPlayer():
             idCol=smart_AI(board)
         draw(board,idCol)
         
-############################################################################
-# Help menu
-############################################################################  
-def printRules():
-    '''
-    Function used inside GUI to generate a window that displays the rules of the game 
-    
-    Loads a text files 'rules_eng.txt' saved inside the same directory.
-    Opens a second window.
-    Writes the content of the text document 
-    
-    Returns
-    -------
-    None.
 
-    '''
-    ruleWindow=tk.Toplevel()
-    ruleWindow.title("How to play")
-    with open('rules_eng.txt') as f:
-        gameRules=f.read()
-    lab_Rule=tk.Label(ruleWindow,text=gameRules,fg="black", anchor="e", justify=tk.LEFT)
-    lab_Rule.pack(side=tk.TOP)
-    ruleWindow.mainloop()
-
-
-def about():
-    aboutWindow=tk.Toplevel()
-    aboutWindow.title("About") 
-    with open('about.txt') as f:
-        about=f.read()
-    lbl_about=tk.Label(aboutWindow,text=about,fg="black", anchor="e", justify=tk.LEFT)
-    lbl_about.pack(side=tk.TOP)
-    aboutWindow.mainloop()  
 ############################################################################
 # Graphics window
 ############################################################################
@@ -202,8 +173,8 @@ settingsMenu.add_command(label='Intermediate',command=difficulty_intermediate)
 
 helpMenu = tk.Menu(top, tearoff=False)
 top.add_cascade(label='Help', menu=helpMenu)
-helpMenu.add_command(label='How to play?',command=printRules)
-helpMenu.add_command(label='About',command=about)
+helpMenu.add_command(label='How to play?', command=print_rules)
+helpMenu.add_command(label='About', command=about)
 
 ############################################################################
 # Other
