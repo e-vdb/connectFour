687a1549a02eef09590fd5350ade0d19db7bae7a
evdb <emelinevanderbeken@gmail.com>
2021-11-30 09:47:44 +0100

    Deleted create_board function

--- connectFour/connectFour.py
+++ connectFour/connectFour.py
@@ -1,24 +1,8 @@
 """
 Connect four game functions.
 """
-import numpy as np
-
-
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-
-def create_board() -> None:
-    """
-    Create a board.
-
-    Returns
-    -------
-    board : np.array
-        Array that stores the board of the connect Four game.
-
-    """
-    board=np.zeros((ROW_COUNT, COLUMN_COUNT))
-    return board
 
 def valid_move(board, col) -> bool :
     """
